3f381f12af4c7707b8c5827e394da63cda4c495e
asa9874 <pjkpjk1129@gmail.com>
2024-07-24 01:07:28 +0900

    기본적인 틀만 만듬

---
+++ main.js
@@ -0,0 +1,12 @@
+import './reset.css'
+import './style.css'
+
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import $ from 'jquery'
+
+
+$('#app').text('asdasd');
+
+
+setupCounter(document.querySelector('#counter'))
