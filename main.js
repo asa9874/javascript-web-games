189f67cdb3e6aa09e7110db71c08f30125d23965
asa9874 <pjkpjk1129@gmail.com>
2024-07-24 22:04:09 +0900

    애니메이션 효과 만듬

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 import './reset.css'
 import './style.css'
-
+import './animation.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import $ from 'jquery'
 
 
-$('#app').text('asdasd');
+//버튼 누를때
+$('.box1').on('click', function() {
+    $('.year1').toggleClass('visible');
+    $('.year2').toggleClass('visible');
+    setTimeout(function() {
+        $('.year2').toggleClass('visible'); 
+        $('.year1').toggleClass('visible');
+    }, 3000);
 
+    //에니매이션
+    $(this).removeClass('animatemove1');
+    $(this).removeClass('animatehidden');
+    $('.box2').removeClass('animatehidden');
+    void this.offsetWidth; 
+    void $('.box2').offsetWidth; 
+    $(this).addClass('animatemove1');
+    $('.box2').addClass('animatehidden');
 
-setupCounter(document.querySelector('#counter'))
+    
+})
+
+//버튼 누를때
+$('.box2').on('click', function() {
+    $('.year1').toggleClass('visible');
+    $('.year2').toggleClass('visible');
+    setTimeout(function() {
+        $('.year2').toggleClass('visible'); 
+        $('.year1').toggleClass('visible'); 
+    }, 3000);
+
+    
+    //에니매이션
+    $(this).removeClass('animatemove2');
+    $(this).removeClass('animatehidden');
+    $('.box1').removeClass('animatehidden');
+    void this.offsetWidth; 
+    void $('.box1').offsetWidth; 
+
+    $(this).addClass('animatemove2');
+    $('.box1').addClass('animatehidden');
+})
