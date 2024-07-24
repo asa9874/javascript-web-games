bdaa80c8653fc9a0b0b85a3d48941ca58e1cc5c0
asa9874 <pjkpjk1129@gmail.com>
2024-07-25 01:02:15 +0900

    랜덤으로 바뀌게 설정

--- main.js
+++ main.js
@@ -4,46 +4,74 @@ import './animation.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import $ from 'jquery'
+import { ANIMATIONS } from './animation'
+
+const $box1 = $('.box1');
+const $box2 = $('.box2');
+
+const $year1 =$('.year1');
+const $year2 =$('.year2');
 
 
-//버튼 누를때
+
+//랜덤 애니 선택함수
+function randomAnime(){
+    $('.box').each(function() {
+        const index = Math.floor(Math.random() * ANIMATIONS.length);
+        $(this).find('.anime-img').attr('src', ANIMATIONS[index].AnimationImg);
+        $(this).find('.anime-year').text(ANIMATIONS[index]['AnimationYear'])
+        $(this).find('.anime-name').text(ANIMATIONS[index]['Animationname'])
+    });
+}
+
+
+//애니 나온 년도 보이게하는 함수
+function AnimeYearVisible(){
+    $year1.toggleClass('visible');
+    $year2.toggleClass('visible');
+    setTimeout(function() {
+        $year2.toggleClass('visible'); 
+        $year1.toggleClass('visible');
+        randomAnime();
+    }, 3000);
+}
+
+
+//애니메이션 지우는 작업
+function RemoveAnimation(){
+    $box1.removeClass('animatemove1');
+    $box1.removeClass('animatehidden');
+    $box2.removeClass('animatemove2');
+    $box2.removeClass('animatehidden');
+    
+    void $box1[0].offsetWidth; 
+    void $box2[0].offsetWidth; 
+}
+
+
+
+
+
+//버튼 누를때1
 $('.box1').on('click', function() {
-    $('.year1').toggleClass('visible');
-    $('.year2').toggleClass('visible');
-    setTimeout(function() {
-        $('.year2').toggleClass('visible'); 
-        $('.year1').toggleClass('visible');
-    }, 3000);
+    RemoveAnimation()
+    AnimeYearVisible()
 
-    //에니매이션
-    $(this).removeClass('animatemove1');
-    $(this).removeClass('animatehidden');
-    $('.box2').removeClass('animatehidden');
-    void this.offsetWidth; 
-    void $('.box2').offsetWidth; 
-    $(this).addClass('animatemove1');
-    $('.box2').addClass('animatehidden');
+    $box1.addClass('animatemove1');
+    $box2.addClass('animatehidden');
 
-    
 })
 
-//버튼 누를때
+//버튼 누를때2
 $('.box2').on('click', function() {
-    $('.year1').toggleClass('visible');
-    $('.year2').toggleClass('visible');
-    setTimeout(function() {
-        $('.year2').toggleClass('visible'); 
-        $('.year1').toggleClass('visible'); 
-    }, 3000);
-
+    RemoveAnimation()
+    AnimeYearVisible()
     
-    //에니매이션
-    $(this).removeClass('animatemove2');
-    $(this).removeClass('animatehidden');
-    $('.box1').removeClass('animatehidden');
-    void this.offsetWidth; 
-    void $('.box1').offsetWidth; 
-
-    $(this).addClass('animatemove2');
-    $('.box1').addClass('animatehidden');
+    $box2.addClass('animatemove2');
+    $box1.addClass('animatehidden');
 })
+
+
+
+
+
