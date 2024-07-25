d2081252ca145c075889c1216fc0b17685018d6f
asa9874 <pjkpjk1129@gmail.com>
2024-07-26 00:42:43 +0900

    애니메이션,게임패배조건,정답확인구현

--- animation.js
+++ animation.js
@@ -1,10 +1,10 @@
 export const ANIMATIONS=[
-    {id:1,Animationname:"애니이름1",AnimationImg:"https://media1.tenor.com/m/62BV0H4Pnq8AAAAd/anime.gif",AnimationYear:"2024-05-1"},
-    {id:2,Animationname:"애니이름2",AnimationImg:"https://media.tenor.com/Qpa94KC4rmcAAAAM/anime-anime-gif.gif",AnimationYear:"2024-05-2"},
-    {id:3,Animationname:"애니이름3",AnimationImg:"https://media.tenor.com/2YH-tnAc9foAAAAM/olive-high-guardian-spice.gif",AnimationYear:"2024-05-3"},
-    {id:4,Animationname:"애니이름4",AnimationImg:"https://media.tenor.com/xU2Gm3PWBi0AAAAM/rapidtulip647-rt6.gif",AnimationYear:"2024-05-4"},
-    {id:5,Animationname:"애니이름5",AnimationImg:"https://media.tenor.com/Hw9S0wns0jwAAAAM/gojo-gojo-satoru.gif",AnimationYear:"2024-05-5"},
-    {id:6,Animationname:"애니이름6",AnimationImg:"https://media.tenor.com/x2aTDE_l-7kAAAAM/tokoyami-towa-novice.gif",AnimationYear:"2024-05-6"},
-    {id:7,Animationname:"애니이름7",AnimationImg:"https://media.tenor.com/E1EW9pwYAz4AAAAM/frieren-anime.gif",AnimationYear:"2024-05-7"},
+    {id:1,Animationname:"애니이름1",AnimationImg:"https://media1.tenor.com/m/62BV0H4Pnq8AAAAd/anime.gif",AnimationYear:"2024-05-01"},
+    {id:2,Animationname:"애니이름2",AnimationImg:"https://media.tenor.com/Qpa94KC4rmcAAAAM/anime-anime-gif.gif",AnimationYear:"2024-05-02"},
+    {id:3,Animationname:"애니이름3",AnimationImg:"https://media.tenor.com/2YH-tnAc9foAAAAM/olive-high-guardian-spice.gif",AnimationYear:"2024-05-03"},
+    {id:4,Animationname:"애니이름4",AnimationImg:"https://media.tenor.com/xU2Gm3PWBi0AAAAM/rapidtulip647-rt6.gif",AnimationYear:"2024-05-04"},
+    {id:5,Animationname:"애니이름5",AnimationImg:"https://media.tenor.com/Hw9S0wns0jwAAAAM/gojo-gojo-satoru.gif",AnimationYear:"2024-05-05"},
+    {id:6,Animationname:"애니이름6",AnimationImg:"https://media.tenor.com/x2aTDE_l-7kAAAAM/tokoyami-towa-novice.gif",AnimationYear:"2024-05-06"},
+    {id:7,Animationname:"애니이름7",AnimationImg:"https://media.tenor.com/E1EW9pwYAz4AAAAM/frieren-anime.gif",AnimationYear:"2024-05-07"},
 
 ]
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './reset.css'
 import './style.css'
 import './animation.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import $ from 'jquery'
 import { ANIMATIONS } from './animation'
 
@@ -12,7 +10,13 @@ const $box2 = $('.box2');
 const $year1 =$('.year1');
 const $year2 =$('.year2');
 
+let year1Date=new Date($year1.text()).getTime()
+let year2Date=new Date($year2.text()).getTime()
 
+const $LifeCount = $('.LifeCount')
+
+
+$('.life').addClass('animatebounce');
 
 //랜덤 애니 선택함수
 function randomAnime(){
@@ -22,6 +26,8 @@ function randomAnime(){
         $(this).find('.anime-year').text(ANIMATIONS[index]['AnimationYear'])
         $(this).find('.anime-name').text(ANIMATIONS[index]['Animationname'])
     });
+    year1Date=new Date($year1.text()).getTime()
+    year2Date=new Date($year2.text()).getTime()
 }
 
 
@@ -62,7 +68,34 @@ function RemoveAnimation(){
 }
 
 
+//상자움직이기
+function MoveBox(){
+    if(year1Date<=year2Date){
+        $box1.addClass('animatemove1');
+        $box2.addClass('animatehidden');
+    }
+    else{
+        $box2.addClass('animatemove2');
+        $box1.addClass('animatehidden');
+    }
+}
 
+
+//정답확인
+function CheckCorrect(box){
+    const $box = $(box);
+    const lifeCountValue =parseInt($LifeCount.text(), 10)
+
+    if($box.is($box1) && year1Date>year2Date){
+        $LifeCount.text(lifeCountValue-1);
+    }
+    if($box.is($box2) && year2Date>year1Date){
+        $LifeCount.text(lifeCountValue-1);
+    }
+    if (parseInt($LifeCount.text(), 10) === 0) {
+        window.location.href = 'https://www.youtube.com/watch?v=khnNHHGR8jw';
+    }
+}
 
 
 //버튼 누를때1
@@ -70,10 +103,8 @@ $('.box1').on('click', function() {
     Clickable();
     RemoveAnimation()
     AnimeYearVisible()
-
-    $box1.addClass('animatemove1');
-    $box2.addClass('animatehidden');
-
+    MoveBox()
+    CheckCorrect(this)
 })
 
 //버튼 누를때2
@@ -81,12 +112,21 @@ $('.box2').on('click', function() {
     Clickable();
     RemoveAnimation()
     AnimeYearVisible()
-    
-    $box2.addClass('animatemove2');
-    $box1.addClass('animatehidden');
+    MoveBox()   
+    CheckCorrect(this)
 })
 
 
 
-
-
+//시작할때
+$(function() {
+    randomAnime()
+    $box1.addClass('animatestartgamebox');
+    $box2.addClass('animatestartgamebox');
+    setTimeout(function() {
+        $box1.removeClass('animatestartgamebox');
+        $box2.removeClass('animatestartgamebox');
+        void $box1[0].offsetWidth; 
+        void $box2[0].offsetWidth; 
+    }, 2000);
+});
