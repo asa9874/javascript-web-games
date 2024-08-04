c9ec589a56a9b3b73e4f7824e8cd3d5cfb1d6ccf
asa9874 <pjkpjk1129@gmail.com>
2024-08-04 14:45:51 +0900

    프로젝트 이름변경으로 인한 설정변경

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-base: '/Visual-Novel-Game/', // GitHub Pages의 서브 디렉토리 경로
+base: '/Text-Adventure-Game/', // GitHub Pages의 서브 디렉토리 경로
 });
