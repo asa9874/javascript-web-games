04d23b7b1760921f27bbcb43c1cab7ce6d7d1d98
asa9874 <pjkpjk1129@gmail.com>
2024-08-14 23:57:37 +0900

    틀제작

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/reset.css'
+import './assets/style.css'
+import $ from 'jquery'
+
+
+$('.studentcardimg').css('background-image',"url('./img/1.png')")
+$('.characterbox').css('background-image',"url('./img/door.png')")
