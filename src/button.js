b4923cf8d3960daf256aca3ee48fc23a551a199f
asa9874 <pjkpjk1129@gmail.com>
2024-08-16 03:13:59 +0900

    이미지 추가, 이미지 변경기능추가

--- src/button.js
+++ src/button.js
@@ -9,20 +9,25 @@ $('.choosebutton').on('click', function() {
     }, 200);
 });
 
-
+//예스버튼
 $('.yesbutton').on('click', function() {
   $('.studentcard').removeClass('show').addClass('hide');
   $('.character').removeClass('come').addClass('pass');
+  $('.choosebutton').css("pointer-events", "none");
   setTimeout(function() {NextPerson()},1000)
 });
 
+
+//노버튼
 $('.nobutton').on('click', function() {
   $('.studentcard').removeClass('show').addClass('hide');
   $('.character').removeClass('come').addClass('unpass');
+  $('.choosebutton').css("pointer-events", "none");
   setTimeout(function() {NextPerson()},1000)
 });
 
 
+//디버그버튼
 $('.selectbutton5').on('click', function() {
   NextPerson()
 });
@@ -32,9 +37,37 @@ function NextPerson(){
     $('.studentcard').removeClass('hide').addClass('show');
     $('.character').hide()
     $('.character').removeClass('come').addClass('unpass');
+    ChangePerson()
+    
+    
+    setTimeout(function() {
+      $('.choosebutton').css("pointer-events", "auto");
+    }, 800);
+
     setTimeout(function() {
       $('.character').show()
       $('.character').removeClass('unpass').removeClass('pass').addClass('come');
       $('.studentcard').removeClass('hide').addClass('show');
+      
     }, 100);
-  }
+
+    $('.angryNotification').removeClass('hide').addClass('show');
+    setTimeout(function() {
+      $('.angryNotification').removeClass('show').addClass('hide');
+    },2000);
+
+}
+
+
+//사람바꾸기
+function ChangePerson(){
+  const school = ["maid", "soilder", "witch"];
+  const backcolor={"maid":"rgb(209, 208, 208)", "soilder":"rgb(110, 219, 125)", "witch":"rgb(238, 149, 238)"}
+  const randomSchool  = school[Math.floor(Math.random() * school.length)];
+  const randomNumber = Math.floor(Math.random() * 20) + 1;
+  var studentimg=`./img/character/${randomSchool}/${randomSchool} (${randomNumber}).png`
+  
+  $('.studentcardimg').css('background-image',`url('${studentimg}')`)
+  $('.studentcardimg').css('background-color',`${backcolor[randomSchool]}`)
+  $('.character').attr('src',studentimg)
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './assets/reset.css'
 import './assets/style.css'
 import $ from 'jquery'
 
-
-$('.studentcardimg').css('background-image',"url('./img/1.png')")
+//이미지 설정
+$('.studentcardimg').css('background-image',"url('./img/character/witch/witch (4).png')")
 $('.characterbox').css('background-image',"linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.5)),url('./img/wall3.png')")
-$('.character').attr('src',"./img/character2.png")
+$('.character').attr('src',"./img/character/witch/witch (4).png")
 $('.yesimg').attr('src',"./img/yes.png")
 $('.noimg').attr('src',"./img/no.png")
+$('.angrychairman').attr('src',"./img/angry.png")
