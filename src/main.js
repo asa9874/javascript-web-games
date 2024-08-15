65344a7bb8792eaa3354a217136f8927dc6e116f
asa9874 <pjkpjk1129@gmail.com>
2024-08-15 17:58:52 +0900

    ui 추가

--- src/main.js
+++ src/main.js
@@ -5,3 +5,26 @@ import $ from 'jquery'
 
 $('.studentcardimg').css('background-image',"url('./img/1.png')")
 $('.characterbox').css('background-image',"url('./img/door.png')")
+
+
+$('.choosebutton').on('click', function() {
+    var $button = $(this);
+
+    $button.addClass('click');
+
+    // 클릭 클래스를 0.2초 후 제거
+    setTimeout(function() {
+      $button.removeClass('click');
+    }, 200);
+});
+
+
+$('.yesbutton').on('click', function() {
+  var $studentCard = $('.studentcard');
+  $studentCard.removeClass('hide').addClass('show');
+});
+
+$('.nobutton').on('click', function() {
+  var $studentCard = $('.studentcard');
+    $studentCard.removeClass('show').addClass('hide');
+});
