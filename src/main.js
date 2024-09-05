5c4ddfe8cc4a977994ea4f99a238c3511e25c141
asa9874 <pjkpjk1129@gmail.com>
2024-09-05 12:38:22 +0900

    frame

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import './styles/style.css';
+
+
+const $advertise = $('.advertise')
+
+$advertise.on('click', function() {
+  window.open(`https://www.youtube.com/watch?v=RtPwBk0pqKE`);
+})
