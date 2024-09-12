3c180f6c1d970738292fe8e1114664d39ec47679
asa9874 <pjkpjk1129@gmail.com>
2024-09-12 15:21:59 +0900

    바이러스광고

--- src/bettery.js
+++ src/bettery.js
@@ -2,5 +2,5 @@ import $ from 'jquery';
 
 
 
-$('.GoogleImg').css('background-image',"url('./bettery/google.png')")
-$('.robotimg').css('background-image',"url('./bettery/robot.jpg')")
+$('.GoogleImg').css('background-image',"url('./google.png')")
+$('.robotimg').css('background-image',"url('./robot.jpg')")
